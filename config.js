527a2483ef1fe1f10cd626ae263d37567a002cdd
Oldrich Kruchna <kruchna.o@gmail.com>
2017-10-21 12:20:48 +0200

    Finish visual, add mobile optimization, add configuration

---
+++ config.js
@@ -0,0 +1,27 @@
+window.people = [
+    {
+        name: 'Johnny Smile',
+        password: 'newyork',
+        secretInformation: 'Takže už mám našetřeno na cestu 48 000 Kč. Můj bakovní účet: 14151515125/0800 Moje heslo do banky: newyork. Babička má narozeniny 22.10. - koupit kytku a bonbonieru!!!'
+    },
+    {
+        name: 'Červená Karkulka',
+        password: 'cervena',
+        secretInformation: 'Včera jsem ten košík babičce nedonesla, bábovku jsem snědla a víno jsem vypila s vlkem.'
+    },
+    {
+        name: 'Lenka Fialková',
+        password: 'pepinka',
+        secretInformation: 'Nesnáším kočky, tu fotku jsem si udělala jenom proto, abych vypadala dobře na Facebooku. Pořád mám všude ty chlupy a mám z toho alergii. Fuj. Raději psa.'
+    },
+    {
+        name: 'Pudl Luboš',
+        password: 'jezevciceBea',
+        secretInformation: 'Zítra mám rande s čivavou Nelou, doufám, že to Bea nezjistí. Haf.'
+    },
+    {
+        name: 'Leoš Hokejka',
+        password: 'JardaJagr68',
+        secretInformation: 'Mám špatné svědomí, ten třetí gól v Kladně jsem nedal já, ale nikdo to neví. Dal to Josef hokejkou, ale díval se jinam. Neví o tom. Mám mu to říct? Asi ne.'
+    }
+];
